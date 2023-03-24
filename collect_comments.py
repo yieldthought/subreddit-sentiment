84d634e7c8d8bce0f6110d322303c5013c323643
Mark O'Connor <moconnor@tenstorrent.com>
2023-03-24 10:04:22 +0100

    Task 7 done

--- collect_comments.py
+++ collect_comments.py
@@ -1,22 +1,24 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 
-with open('subreddits.txt', 'r') as f:
-    subreddits = [line.strip() for line in f.readlines()]
+def collect_comments_from_subreddits():
+    with open('subreddits.txt', 'r') as f:
+        subreddits = [line.strip() for line in f.readlines()]
 
-num_comments = 10
+    num_comments = 10
+    comments = {}
+    for subreddit in subreddits:
+        url = f'https://www.reddit.com/r/{subreddit}/comments.json?limit={num_comments}'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        data = json.loads(response.text)
 
-comments = {}
-for subreddit in subreddits:
-    url = f'https://www.reddit.com/r/{subreddit}/comments.json?limit={num_comments}'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
+        comments[subreddit] = []
+        for comment in data['data']['children']:
+            comments[subreddit].append(comment['data']['body'])
 
-    comments[subreddit] = []
-    for comment in data['data']['children']:
-        comments[subreddit].append(comment['data']['body'])
+    with open('comments.json', 'w') as f:
+        json.dump(comments, f)
 
-with open('comments.json', 'w') as f:
-    json.dump(comments, f)
+if __name__ == '__main__':
+    collect_comments_from_subreddits()
